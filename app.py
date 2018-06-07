36845eeecc79b6bb22bd5fff7d7e707bc93d4daf
wsq <mywsq961@gmail.com>
2018-06-07 19:08:41 +0800

    跨域设置, 更新依赖

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, abort,jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+# 允许跨域访问
+CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/test")
+def connect_test():
+    test_str = request.args.get('testStr')
+    # 如果收到请求字符串则返回同样的字符串
+    if test_str:
+        return test_str
+    # 否则报错
+    else:
+        abort(502)
+        
