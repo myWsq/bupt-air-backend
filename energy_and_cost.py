895d0d2e270fdf3d4455a323622bc86e4571101e
RanFeng <13269868811@163.com>
2018-06-15 15:56:47 +0800

    readme,energy_and_cost.py

--- energy_and_cost.py
+++ energy_and_cost.py
@@ -1,32 +1,47 @@
 import json
 from os import path
 import pymysql
+import time
 
-#计算每一台从机的能量及消费金额并实时更新到从机状态
-def cost(card_id,slave_id):
-	# 读取配置文件
-	f = open(path.join(path.dirname(__file__), 'config.json'), 'r')
-	config = json.load(f)
-	f.close()
-	db = pymysql.connect(**config)
-	cursor = db.cursor()
-	logs={'res_time':[],'speed':[]}#存放该从控机最近两次的日志
-	
-	# 选出该从控机最近两次的日志
-	cursor.execute("select speed,res_time from log where card_id=%s and slave_id=%s order by res_time desc limit 2"%(card_id,slave_id))
-	for log in cursor:
-		logs['res_time'].append(log[1])
-		logs['speed'].append(log[0])
+class costor:
+	def __init__(self):
+		self.flag=True
+		# 读取配置文件
+		f = open(path.join(path.dirname(__file__), 'config.json'), 'r')
+		config = json.load(f)
+		f.close()
+		self.db = pymysql.connect(**config)
+		self.cursor = self.db.cursor()
 
-	sub_time=(logs['res_time'][0]-logs['res_time'][1]).total_seconds()#最近两次request的时间差(以秒为单位)
-	if logs['speed'][1]==1:#上一次request是低速风，按每分钟0.8标准功率计算能量
-		energy=0.8*sub_time/60
-	elif logs['speed'][1]==3:#上一次request是高速风，按每分钟1.3标准功率计算能量
-		energy=1.3*sub_time/60
-	else:#上一次request是中速风，按每分钟1.0标准功率计算能量
-		energy=1.0*sub_time/60
-	cost=5*energy#每标准能量5元
+	#以秒为单位计算每一台从机的能量及消费金额并更新到从机状态
+	def run(self):
+		while self.flag:
+			status={'id':[],'speed':[], 'amount':[]}#存放每一台从控机的状态信息
 
-	cursor.execute("update status set energy=energy+%s,amount=amount+%s where id=%s"%(energy,cost,slave_id))
-	db.commit()
-	db.close()
+			# 选出每一台从控机的状态信息
+			self.cursor.execute("select id,speed, amount from status where speed!=0")
+			for sta in self.cursor:
+				status['id'].append(sta[0])
+				status['speed'].append(sta[1])
+				status['amount'].append(sta[2])
+
+			#计算每一台从控机的单位时间消费金额
+			for num in range(len(status['id'])):
+				if status['speed'][num]==1:#上一次request是低速风，按每分钟0.8标准功率计算能量
+					energy=0.8*1/60
+				elif status['speed'][num]==3:#上一次request是高速风，按每分钟1.3标准功率计算能量
+					energy=1.3*1/60
+				else:#上一次request是中速风，按每分钟1.0标准功率计算能量
+					energy=1.0*1/60
+				cost=5*energy#每标准能量5元
+
+				#更新从控机状态
+				self.cursor.execute("update status set energy=energy+%s,amount=amount+%s where id=%s"%(energy,cost,status['id'][num]))
+
+			self.db.commit()#提交
+			time.sleep(1)#为实现一秒计算一次
+
+	def exit(self):
+		self.flag=False#为退出计算
+		time.sleep(2)#为了防止最新一次更新未提交
+		self.db.close()
